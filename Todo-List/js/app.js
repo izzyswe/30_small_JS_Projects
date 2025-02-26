81f6ee038283b675417dc2d03dc0e8e923aea415
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-02-26 00:36:45 -0500

    added click functionality

--- Todo-List/js/app.js
+++ Todo-List/js/app.js
@@ -2,10 +2,26 @@
 const inputTask = document.getElementById('input');
 const submitForm = document.getElementById('form');
 const ultaskList = document.getElementById('UList');
-const li = document.CreateElement('li');
+const taskList = document.getElementById('list');
+//const li = document.createElement('li');
 //const taskList = document.getElementById('task-list');
 
 // Event Listener
-function addTask(){
+submitForm.addEventListener('submit', function(e){
+  console.log('clicked');
+  e.preventDefault();  
+  //creating an element
+    const li = document.createElement('li');
+    const addBtn = document.createElement('button');
+    const addBtnText = document.createTextNode('Add');
+    addBtn.appendChild(addBtnText);
+   li.appendChild(addBtn);
+  taskList.appendChild(li);
 
-}
+    //const task = inputTask.value;
+    //const li = document.createElement('li');
+    //li.appendChild(document.createTextNode(task));
+    //ultaskList.appendChild(li);
+    //inputTask.value = '';
+    //console.log(task);
+});
